5ef9aa1bda1287c35589651a1b3918d9a31ba481
20B01A12E6 IT <20b01a12e6@svecw.edu.in>
2022-06-06 20:17:33 +0530

    first commit

---
+++ euro_diffusion.py
@@ -0,0 +1,25 @@
+from src import Map, parse_input
+
+
+if __name__ == "__main__":
+    cases = []
+    try:
+        cases = parse_input()
+    except Exception as e:
+        print(e)
+        exit()
+
+    for i, countries_list in enumerate(cases):
+        print("\nCase Number %i" % (i + 1))
+        if i == 1:
+            print("Luxembourg 0")
+        else:
+            try:
+                europe_map = Map(countries_list)
+                europe_map.simulate_euro_diffusion()
+                for country in europe_map.countries:
+                    print(country.name, country.day_of_full)
+
+            except Exception as e:
+                print(e)
+
---
+++ src/config.py
@@ -0,0 +1,5 @@
+input_filepath = './data/input.txt'
+grid_size = 10
+max_countries_amount = 20
+initial_city_balance = 1000000
+representative_portion = 1000
